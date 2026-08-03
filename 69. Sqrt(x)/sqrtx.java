d3dd4d877bc6e0f8b99a1b919607de5523f0bc4e
George Assaad <lopgogo@gmail.com>
2026-08-03 11:06:07 +0300

    Add sqrtx.java

---
+++ 69. Sqrt(x)/sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+        int l = 1;
+        int r = x / 2 + 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            long square = (long) mid * mid; // changes the first operand:
+            if (square == x) {
+                return mid;
+            } else if (square < x){
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return r;
+    }
+}
